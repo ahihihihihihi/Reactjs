9400e4fbb4e324e90f42e7d42b8e774f0070e5ae
ahihi <bdsthocu20@gmail.com>
2023-05-16 17:20:35 +0700

    #40

--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -3,6 +3,7 @@ import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
 import './userManage.scss';
 import { getAllUsers } from '../../services/userService'
+import ModalUser from './ModalUser';
 
 
 class UserManage extends Component {
@@ -10,7 +11,8 @@ class UserManage extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            arrUsers: []
+            arrUsers: [],
+            isOpenModalUser: false
         }
     }
 
@@ -24,14 +26,33 @@ class UserManage extends Component {
         //console.log('get users from nodejs: ', response);
     }
 
+    handleAddNewUser = () => {
+        this.setState({
+            isOpenModalUser: true
+        })
+    }
+
+    toggleUserModal = () => {
+        this.setState({
+            isOpenModalUser: !this.state.isOpenModalUser
+        })
+    }
 
     render() {
-        console.log('check render: ', this.state);
+        // console.log('check render: ', this.state);
         let arrUsers = this.state.arrUsers;
         return (
             <div className="user-container">
+                <ModalUser
+                    isOpen={this.state.isOpenModalUser}
+                    toggleFromParent={this.toggleUserModal}
+                    test={'abc'}
+                />
                 <div className='title text-center'>
                     Manage users with ahihi
+                </div>
+                <div className='mx-1'>
+                    <button className='btn btn-primary px-3' onClick={() => this.handleAddNewUser()}><i className="fas fa-plus"></i> Add new user</button>
                 </div>
                 <div className='users-table mt-3 mx-1'>
                     <table id="customers">
@@ -43,7 +64,7 @@ class UserManage extends Component {
                             <th>Actions</th>
                         </tr>
                         {arrUsers && arrUsers.map((item, index) => {
-                            console.log('loop users: ', item, index);
+                            // console.log('loop users: ', item, index);
                             return (
                                 <>
                                     <tr>
