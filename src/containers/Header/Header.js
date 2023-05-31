620d76a3ad0e503968c168ad125c48b07262eeb8
ahihi <bdsthocu20@gmail.com>
2023-05-31 17:46:36 +0700

    #55

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -6,6 +6,7 @@ import Navigator from '../../components/Navigator';
 import { adminMenu } from './menuApp';
 import './Header.scss';
 import { LANGUAGES } from '../../utils';
+import { FormattedMessage } from 'react-intl';
 
 
 class Header extends Component {
@@ -15,7 +16,8 @@ class Header extends Component {
     }
 
     render() {
-        const { processLogout, language } = this.props;
+        const { processLogout, language, userInfo } = this.props;
+        // console.log('check userinfo: ', userInfo);
         return (
             <div className="header-container">
                 {/* thanh navigator */}
@@ -23,6 +25,7 @@ class Header extends Component {
                     <Navigator menus={adminMenu} />
                 </div>
                 <div className='languages'>
+                    <div className='welcome'><FormattedMessage id="home-header.welcome" />, {userInfo.firstName ? userInfo.firstName : ''}! </div>
                     <div className={language === LANGUAGES.VI ? 'language-vi active' : 'language-vi'}><span onClick={() => { this.changeLanguage(LANGUAGES.VI) }}>VN</span></div>
                     <div className={language === LANGUAGES.EN ? 'language-en active' : 'language-en'}><span onClick={() => { this.changeLanguage(LANGUAGES.EN) }}>EN</span></div>
                     {/* nút logout */}
@@ -39,7 +42,8 @@ class Header extends Component {
 const mapStateToProps = state => {
     return {
         isLoggedIn: state.user.isLoggedIn,
-        language: state.app.language
+        language: state.app.language,
+        userInfo: state.user.userInfo
     };
 };
 
