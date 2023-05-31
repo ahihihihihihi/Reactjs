88a503cf132e020a1a036da99e2e684fcd08a3e6
ahihi <bdsthocu20@gmail.com>
2023-05-31 14:51:25 +0700

    #54

--- src/containers/HomePage/Section/About.js
+++ src/containers/HomePage/Section/About.js
@@ -17,7 +17,7 @@ class About extends Component {
                     </div>
                     <div className='section-body'>
                         <div className='about-video'>
-                            <iframe width="100%" height="400" src="https://www.youtube.com/embed/147SkAVXEqM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                            <iframe width="100%" height="400" src="https://www.youtube.com/embed/147SkAVXEqM" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
                         </div>
                         <div className='about-text'>
                             Trong video này, chúng ta sẽ hoàn tất việc design giao diện theo trang bookingcare.vn. Chúng ta sẽ hoàn thiện những phần đang còn dang dở, để từ video tiếp theo, chúng ta sẽ bắt đầu làm về backend và react để tạo dữ liệu thật cho trang home design này.
