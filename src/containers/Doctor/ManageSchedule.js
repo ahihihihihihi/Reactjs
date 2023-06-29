bf396f457f2a0b440d73885f5482f53aac285982
ahihi <bdsthocu20@gmail.com>
2023-06-29 17:29:52 +0700

    #72.1

--- src/containers/Doctor/ManageSchedule.js
+++ src/containers/Doctor/ManageSchedule.js
@@ -1,23 +1,41 @@
 import React, { Component, Fragment } from 'react';
 import { connect } from "react-redux";
-
+import { FormattedMessage } from 'react-intl';
+import './ManageSchedule.scss';
 
 class Doctor extends Component {
     render() {
-        const {isLoggedIn } = this.props;
         return (
-            <React.Fragment>
-                <div className='manage-schedule'>
-                    manage schedule.
+                <div className='manage-schedule-container'>
+                    <div className='m-s-title'>
+                        <FormattedMessage id="manage-schedule.title" />
+                    </div>
+                    <div className='container'>
+                        <div className='row'>
+                            <div className='col-6'>
+                                <label>
+                                    Chọn Bác sĩ
+                                </label>
+                                <input className='form-control' />
+                            </div>
+                            <div className='col-6'>
+                                <label>
+                                    Chọn ngày
+                                </label>
+                                <input className='form-control' />
+                            </div>
+                            <div className='col-12 pick-hour-container'>
+                                
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </React.Fragment>
         );
     }
 }
 
 const mapStateToProps = state => {
     return {
-        systemMenuPath: state.app.systemMenuPath,
         isLoggedIn: state.user.isLoggedIn
     };
 };
